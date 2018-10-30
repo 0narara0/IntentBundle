3e7dec2f98135b3d00e628fa1a430e43d8b61736
EDU <0narara0@gmail.com>
2018-10-30 11:52:31 +0900

    third

--- app/src/main/java/com/cmy/www/intentbundle/MainActivity.java
+++ app/src/main/java/com/cmy/www/intentbundle/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -14,13 +15,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         Button button = findViewById(R.id.button);
         button.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
-        String text = ((Button)v).getText().toString();
+
+        EditText editTextName = findViewById(R.id.editTextName);
+        String editTextNameString = editTextName.getText().toString();
         Bundle bundle = new Bundle();
-        bundle.putString("SingleName",text);
+        bundle.putString("SingleName",editTextNameString);
         Intent intent = new Intent(this, ExplicitActivity.class);
         intent.putExtras(bundle);
         startActivity(intent);
